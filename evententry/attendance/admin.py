54a18af62ef5ff287e65981ad296b4c768276c78
yash114bansal <yash114bansal@gmail.com>
2024-04-20 23:00:41 +0530

    update admin filters

--- evententry/attendance/admin.py
+++ evententry/attendance/admin.py
@@ -9,6 +9,8 @@ class BaseImportExportAdmin(ImportExportMixin, admin.ModelAdmin):
 
 @admin.register(Student)
 class StudentAdmin(BaseImportExportAdmin):
-    pass
+    list_display = ('name', 'college_email',  'is_hosteler','student_number', 'branch', 'gender', 'is_present_day1', 'is_present_day2', 'is_present_day3', 'is_present_day4')
+    search_fields = ['name', 'student_number']
+    list_filter = ['gender', 'is_present_day1', 'is_present_day2', 'is_present_day3', 'is_present_day4']
 
 admin.site.register(ActiveDay)
